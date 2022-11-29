8ecc605760ba7c75d1fdfc764789aab9328bbf61
fernando robles <advancexz@gmail.com>
2022-11-29 11:53:57 -0800

    upload spring init project

---
+++ spring-boot-example/src/test/java/com/springthree/springbootexample/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springthree.springbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
