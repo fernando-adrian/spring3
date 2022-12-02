769424440186c8229133468e604b32f67937ff3b
fernando robles <advancexz@gmail.com>
2022-12-01 18:29:33 -0800

    + source files uploaded

---
+++ spring-boot-example/src/main/java/com/fernandorobles/Main.java
@@ -0,0 +1,65 @@
+package com.fernandorobles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("api/v1/customers")
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+        String name,
+        String email,
+        Integer age
+    ){
+
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(
+            @RequestBody NewCustomerRequest request,
+            @PathVariable("customerId") Integer id){
+        Customer customerToUpdate = customerRepository.findById(id).get();
+        customerToUpdate.setName(request.name());
+        customerToUpdate.setEmail(request.email());
+        customerToUpdate.setAge(request.age());
+        customerRepository.save(customerToUpdate);
+    }
+
+}
